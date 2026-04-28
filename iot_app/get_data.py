b59ad89b172bee47cc064ce811636244209065c0
Karloz <caguacas.3749@unicesmag.edu.co>
2026-04-28 16:26:55 -0500

    GetData

---
+++ iot_app/get_data.py
@@ -0,0 +1,17 @@
+import serial
+import time
+
+arduino_port = 'COMX'
+baud_rate = 9600
+
+ser = serial.Serial(
+    arduino_port,
+    baud_rate,
+    timeout=1
+)
+
+time.sleep(2)
+
+while True:
+    data = ser.readline().decode('utf-8').rstrip()
+    print(data)
